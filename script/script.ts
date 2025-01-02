4d15297f83fb10dc5152a4a826e377c7c832ee2f
권샘찬/sam <hypemova@gmail.com>
2025-01-02 16:55:13 +0900

    refactor

--- script/script.ts
+++ script/script.ts
@@ -1,9 +1,7 @@
 import * as ts from 'typescript';
 import * as fs from 'fs';
 
-
-async function run() {
-  const filePath = 'src/app2.controller.ts';
+async function run(filePath: string) {
   const sourceCode = fs.readFileSync(filePath, 'utf-8');
   const sourceFile = ts.createSourceFile(
     filePath,
@@ -12,72 +10,87 @@ async function run() {
   );
 
   ts.forEachChild(sourceFile, visit);
-
-  function visit(node: ts.Node) {
-    if (!ts.isClassDeclaration(node)) {
-      return;
-    }
-
-    const classDecorators = node.modifiers.filter((modifier) =>
-      ts.isDecorator(modifier),
-    );
-
-    // console.log(node.members);
-
-    let version: string | undefined;
-    let apiPath: string | undefined;
-    for (const decorator of classDecorators) {
-      const expression = decorator.expression;
-      if (ts.isCallExpression(expression)) {
-        const callExpression = expression;
-
-        if (ts.isIdentifier(callExpression.expression)) {
-          const identifier = callExpression.expression;
-          const decoratorName = identifier.escapedText;
-          if (decoratorName !== 'Controller') {
-            continue;
-          }
-        }
-
-        const argument: ts.StringLiteral = callExpression.arguments.find(
-          (argument) => ts.isStringLiteral(argument),
-        );
-
-
-        const objectLiteralExpression = callExpression.arguments.find((arg) => ts.isObjectLiteralExpression(arg));
-        if (objectLiteralExpression) {
-          const propertyAssignments = objectLiteralExpression.properties.filter((property) => ts.isPropertyAssignment(property))
-          for (const propertyAssignment of propertyAssignments) {
-            let type: 'version' | 'path' | undefined;
-            const propertyName = propertyAssignment.name;
-            if (ts.isIdentifier(propertyName)) {
-              const escapedText = propertyName.escapedText;
-              if (escapedText === 'version') {
-                type = 'version';
-              } else if (escapedText === 'path') {
-                type = 'path';
-              }
-            }
-            const initializer = propertyAssignment.initializer;
-
-            if (ts.isStringLiteral(initializer)) {
-              if (type === 'version') {
-                version = initializer.text;
-              } else if (type === 'path') {
-                apiPath = initializer.text;
-              }
-            }
-          }
-        }
-
-        if (argument) {
-          apiPath = argument.text;
-        }
-      }
-    }
-    console.log(version);
-    console.log(apiPath);
-  }
 }
 
-run();
+function visit(node: ts.Node) {
+  if (!ts.isClassDeclaration(node)) {
+    return;
+  }
+
+  const classDecorators =
+    node.modifiers?.filter((modifier) => ts.isDecorator(modifier)) || [];
+
+  let version: string | undefined;
+  let apiPath: string | undefined;
+  for (const decorator of classDecorators) {
+    const expression = decorator.expression;
+    if (ts.isCallExpression(expression)) {
+      // 컨트롤러 데코레이터 정보 읽어오기
+      if (isControllerDecorator(expression)) {
+        apiPath = extractPathArgument(expression);
+        const properties = extractObjectLiteralProperties(expression);
+        version = properties.version;
+        apiPath = properties.path;
+      }
+
+      // 메서드 데코레이터 정보 읽어오기
+    }
+  }
+
+  console.log(version);
+  console.log(apiPath);
+}
+
+// 특정 데코레이터가 Controller인지 확인하는 함수
+function isControllerDecorator(callExpression: ts.CallExpression): boolean {
+  if (ts.isIdentifier(callExpression.expression)) {
+    return callExpression.expression.escapedText === 'Controller';
+  }
+  return false;
+}
+
+// 문자열 인자 추출 함수
+function extractPathArgument(
+  callExpression: ts.CallExpression,
+): string | undefined {
+  const argument = callExpression.arguments.find((arg) =>
+    ts.isStringLiteral(arg),
+  );
+  return argument ? (argument as ts.StringLiteral).text : undefined;
+}
+
+// ObjectLiteralExpression에서 속성 추출 함수
+function extractObjectLiteralProperties(callExpression: ts.CallExpression): {
+  version?: string;
+  path?: string;
+} {
+  const objectLiteralExpression = callExpression.arguments.find((arg) =>
+    ts.isObjectLiteralExpression(arg),
+  );
+  if (!objectLiteralExpression) {
+    return {};
+  }
+
+  const properties: { version?: string; path?: string } = {};
+  const propertyAssignments = objectLiteralExpression.properties.filter(
+    (property) => ts.isPropertyAssignment(property),
+  );
+
+  for (const propertyAssignment of propertyAssignments) {
+    const propertyName = propertyAssignment.name;
+    const initializer = propertyAssignment.initializer;
+
+    if (ts.isIdentifier(propertyName) && ts.isStringLiteral(initializer)) {
+      const name = propertyName.escapedText;
+      if (name === 'version') {
+        properties.version = initializer.text;
+      } else if (name === 'path') {
+        properties.path = initializer.text;
+      }
+    }
+  }
+
+  return properties;
+}
+
+run('src/app.controller.ts');
